a26a2817beed3c14e5de3e52c8c38b62b0d3611f
Alexandra <alexswartz9753@gmail.com>
2018-12-11 12:30:31 -0500

    wrote routes for GET and POST

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const hapi = require('hapi');
 const mongoose = require('mongoose');
+const Painting = require('./models/Painting');
 
 mongoose.connect('mongodb://alexswartz:sunnyday4!@ds131814.mlab.com:31814/users');
 
@@ -13,13 +14,35 @@ const server = hapi.server({
 });
 
 const init = async()=> {
-    server.route({
-        method: 'GET',
-        path: '/',
-        handler: function(request, reply) {
-            return `<h1>My Modern API</h1>`;
+    server.route([
+        {
+            method: 'GET',
+            path: '/',
+            handler: function(request, reply) {
+                return `<h1>My Modern API</h1>`;
+            }
+        },
+        {
+            method: 'GET',
+            path: '/api/v1/paintings',
+            handler: (req, reply)=> {
+                return Painting.find();
+            }
+        },
+        {
+            method: 'POST',
+            path: '/api/v1/paintings',
+            handler: (req, reply)=> {
+                const {name, url, techniques} = req.payload;
+                const painting = new Painting({
+                    name,
+                    url,
+                    techniques
+                });
+                return painting.save();
+            }
         }
-    })
+]);
 
     await server.start();
     console.log(`Server running at ${server.info.uri}`);
