a8498eadf18d6cec06dbe0d6d1ce71b2c94d0428
Alexandra <alexswartz9753@gmail.com>
2018-12-11 12:01:22 -0500

    wrote GET route to path /

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ const server = hapi.server({
 });
 
 const init = async()=> {
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: function(request, reply) {
+            return `<h1>My Modern API</h1>`;
+        }
+    })
+
     await server.start();
     console.log(`Server running at ${server.info.uri}`);
 };
